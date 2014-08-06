a99b545ea349dc6344f863bcf92dce2ee23cb4f7
Tomaz Martins <tomaz.unb@outlook.com>
2014-08-06 20:44:12 -0300

    Declarando os principais métodos públicos da classe QuotaDao

--- src/br/com/visualize/akan/api/dao/QuotaDao.java
+++ src/br/com/visualize/akan/api/dao/QuotaDao.java
@@ -4,6 +4,10 @@
  */
 package br.com.visualize.akan.api.dao;
 
+import java.util.List;
+
+import br.com.visualize.akan.domain.model.Quota;
+
 
 /**
  * Serves to define the methods that give the basic functionality to the 
@@ -19,8 +23,8 @@ public class QuotaDao {
 	}
 	
 	/**
-	 * Serves to return the unique instance of QuotaDao active in the project.
-	 * 
+	 * Return the unique instance of QuotaDao active in the project.
+	 * <p>
 	 * @return The unique instance of QuotaDao.
 	 */
 	public static QuotaDao getInstance() {
@@ -34,5 +38,41 @@ public class QuotaDao {
 		}
 		
 		return instanceQuotaDao;
+	}
+	
+	/**
+	 * Inserts in the database quotas, referring to a congressman in particular, 
+	 * passed as parameter in the local database of the application.
+	 * <p>
+	 * @param insertedQuotas List of quotas to be inserted.
+	 */
+	public void insertQuotasOnCongressman( List<Quota> insertedQuotas ) {
+		/*! Write instructions Here. */
+	}
+	
+	/**
+	 * Deletes all quotas of the database relating to the past as parameter 
+	 * congressman for his numerical identifier.
+	 * <p>
+	 * @param idCongressman Numeric identifier of congressman that must have 
+	 * 		 					deleted the quotas.
+	 */
+	public void deleteQuotasFromCongressman( int idCongressman ) {
+		/*! Write instructions Here. */
+	}
+	
+	/**
+	 * Search the database all quotas related to the referenced congressman 
+	 * and returns them as a list.
+	 * <p>
+	 * @param idCongressman Numeric identifier of congressman that must have 
+	 * 		 					deleted the quotas.
+	 * <p>
+	 * @return The list of referenced quotas belonging to the congressman.
+	 */
+	public List<Quota> getQuotasByIdCongressman( int idCongressman ) {
+		/*! Write instructions Here. */
+		
+		return null;
 	}
 }
