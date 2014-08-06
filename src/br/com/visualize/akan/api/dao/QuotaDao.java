19ae679998d61b9169a1bc10d7405265fd553142
Tomaz Martins <tomaz.unb@outlook.com>
2014-08-06 20:22:16 -0300

    Implementando padrão singleton na classe QuotaDao.

--- src/br/com/visualize/akan/api/dao/QuotaDao.java
+++ src/br/com/visualize/akan/api/dao/QuotaDao.java
@@ -1,5 +1,38 @@
+/*
+ * File: 	QuotaDao.java 
+ * Purpose: Brings the implementation of QuotaDao class.
+ */
 package br.com.visualize.akan.api.dao;
 
-public class QuotaDao {
 
+/**
+ * Serves to define the methods that give the basic functionality to the 
+ * database - CRUD - related to parliamentary quotas. This class aims to 
+ * maintain the ability to generate support to the modification of data 
+ * on quotas.
+ */
+public class QuotaDao {
+	private static QuotaDao instanceQuotaDao = null;
+	
+	private QuotaDao( ) {
+		/*! Write instructions Here. */
+	}
+	
+	/**
+	 * Serves to return the unique instance of QuotaDao active in the project.
+	 * 
+	 * @return The unique instance of QuotaDao.
+	 */
+	public static QuotaDao getInstance() {
+		
+		if( instanceQuotaDao != null ) {
+			/*! Nothing To Do. */
+			
+		} else {
+			instanceQuotaDao = new QuotaDao();
+			
+		}
+		
+		return instanceQuotaDao;
+	}
 }
--- src/br/com/visualize/akan/domain/view/DescriptionScreen.java
+++ src/br/com/visualize/akan/domain/view/DescriptionScreen.java
@@ -1,19 +1,32 @@
+/*
+ * File: DescriptionScreen.java 
+ * Purpose: Brings the implementation of DescriptionScreen, a class that 
+ * serves to interface with the user.
+ */
 package br.com.visualize.akan.domain.view;
+
 
 import br.com.visualize.akan.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+/**
+ * Serves to bring the visibility of the detailed data of a congressman. Serves 
+ * to bring the visibility of the detailed data of a congressman. Through this 
+ * class you can give the user a description of the expenses of the share 
+ * congressman.
+ */
 public class DescriptionScreen extends Activity {
 	
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
-	   super.onCreate( savedInstanceState );
-	   setContentView( R.layout.description_screen_activity );
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.description_screen_activity );
 	}
 	
 	@Override
 	protected void onPause() {
-	   super.onPause();
+		super.onPause();
 	}
 }
