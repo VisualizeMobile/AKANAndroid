6e0ac23c6dba3dea9b1f95b28602c2621b3fb16e
Tomaz Martins <tomaz.unb@outlook.com>
2014-08-05 21:51:05 -0300

    Adicionando os métodos callback onCreate e OnPause na DescriptionScreen

--- src/br/com/visualize/akan/domain/view/DescriptionScreen.java
+++ src/br/com/visualize/akan/domain/view/DescriptionScreen.java
@@ -1,7 +1,19 @@
 package br.com.visualize.akan.domain.view;
 
+import br.com.visualize.akan.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class DescriptionScreen extends Activity {
-
+	
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+	   super.onCreate( savedInstanceState );
+	   setContentView( R.layout.description_screen_activity );
+	}
+	
+	@Override
+	protected void onPause() {
+	   super.onPause();
+	}
 }
