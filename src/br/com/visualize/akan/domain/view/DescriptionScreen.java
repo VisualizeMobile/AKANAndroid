3350a403be0b552beff4d2e0de41b03502333aaf
Tomaz Martins <tomaz.unb@outlook.com>
2014-08-10 20:59:46 -0300

    Declarando principais métodos da DescriptionScreen

--- src/br/com/visualize/akan/domain/view/DescriptionScreen.java
+++ src/br/com/visualize/akan/domain/view/DescriptionScreen.java
@@ -9,6 +9,7 @@ package br.com.visualize.akan.domain.view;
 import br.com.visualize.akan.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 /**
@@ -18,6 +19,13 @@ import android.os.Bundle;
  * congressman.
  */
 public class DescriptionScreen extends Activity {
+	private static final String EMPTY_VALUE_QUOTA = "0,00";  // Measured in real.
+	
+	private static final int HIGHER_LIMIT_WHITE_BAR = 500;   // Measured in real.
+	private static final int HIGHER_LIMIT_GRAY_BAR = 1500;   // Measured in real.
+	private static final int HIGHER_LIMIT_GREEN_BAR = 3000; 	// Measured in real.
+	private static final int HIGHER_LIMIT_YELLOW_BAR = 5000; // Measured in real.
+	private static final int LOWER_LIMIT_RED_BAR = 5000; 		// Measured in real.
 	
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -28,5 +36,35 @@ public class DescriptionScreen extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
+	}
+	
+	/**
+	 * Sets the value of each parliamentary quota regarding the Congressman.
+	 */
+	public void setValuesQuotas() {
+		resetValuesQuotas();
+		
+		/*! Write Instructions Here. */
+		
+	}
+	
+	/**
+	 * Sets the size of the bar representing the parliamentary quota.
+	 * <p>
+	 * @param bar Image of the bar should be set.
+	 * @param valueQuota Current value of parliamentary quota you  
+	 * 						want set the bar.
+	 */
+	public void setSizeQuotasBar( ImageView bar, double valueQuota ) {
+		/*! Write Instructions Here. */
+		
+	}
+	
+	/**
+	 * Resets the value of parliamentary quotas, making them equal to zero.
+	 */
+	private void resetValuesQuotas() {
+		/*! Write Instructions Here. */
+		
 	}
 }
