09ff7498cf7159d87f93eb5411dbcceb74300278
MaxAlmeida <llewxam100@hotmail.com>
2014-10-05 20:45:02 -0300

    creating class responsible for manage serial objects in json format

--- src/br/com/visualize/akan/api/helper/JsonHelper.java
+++ src/br/com/visualize/akan/api/helper/JsonHelper.java
@@ -1,5 +1,36 @@
 package br.com.visualize.akan.api.helper;
 
+import java.util.List;
+
+import br.com.visualize.akan.domain.exception.NullCongressmanException;
+import br.com.visualize.akan.domain.model.Congressman;
+import br.com.visualize.akan.domain.model.Quota;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class JsonHelper {
+	
+	public static List<Congressman> listCongressmanFromJSON(String jsonCongressmanList) throws NullCongressmanException{
+		List<Congressman> congressmanList;
+		try{
+			Gson gson = new Gson();
+			congressmanList = gson.fromJson(jsonCongressmanList, new TypeToken<List<Congressman>>(){}.getType());
+		}catch(NullPointerException e){
+			throw new NullCongressmanException();
+		}
+		return congressmanList;
+	}
+	
+	public static List<Quota> listQuotaByIdCongressmanFromJSON(String jsonQuotaByIdCongressmanList) throws NullCongressmanException{
+		List<Quota> quotaList = null;
+		try{
+			Gson gson = new Gson();
+			quotaList = gson.fromJson(jsonQuotaByIdCongressmanList, new TypeToken<List<Quota>>(){}.getType());
+		}catch(NullPointerException e){
+			throw new NullCongressmanException();
+		}
+		return quotaList;
+	}
 
 }
