5dc9ce190452bbf8cfb996cad1d153ec1be134dc
MaxAlmeida <llewxam100@hotmail.com>
2014-11-09 02:29:28 -0200

    Creating menu responsible for search in list from congressmens

--- src/br/com/visualize/akan/domain/view/ListScreen.java
+++ src/br/com/visualize/akan/domain/view/ListScreen.java
@@ -12,29 +12,54 @@ import br.com.visualize.akan.domain.model.Congressman;
 import android.animation.LayoutTransition;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.text.Layout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.Toast;
 
 
 public class ListScreen extends Activity
 {
 	CongressmanController congressmanController;
+	 ListView listView ;
+	 RankingAdapter rankingAdapter;
+	 CongressmenListAdapter listAdapter;
+	 String query;
+	 SearchView search;
+	 
+	
 	@Override
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setContentView(R.layout.list_screen_activity);
-		final ListView listView = (ListView) findViewById( R.id.listView );
+		
+		//handleIntent(getIntent()); 
+	    listView = (ListView) findViewById( R.id.listView );
 		final Button btn_ranking = (Button) findViewById(R.id.btn_ranking);
-		final ArrayAdapter adapter = null;
+		final Button btn_search = (Button) findViewById(R.id.btn_search);
+		
+		search = new SearchView(getApplicationContext());
+		
 		congressmanController =CongressmanController.getInstance(getBaseContext());
 		    
 			congressmanController.getAll();
@@ -42,10 +67,12 @@ public class ListScreen extends Activity
 			congressmen = congressmanController.getCongressmanList();
 			
 		   
-			ViewGroup viewGroup;
-		    final RankingAdapter rankingAdapter = new RankingAdapter(this,R.layout.ranking_layout,congressmen);
-		    final CongressmenListAdapter listAdapter = new CongressmenListAdapter(this,R.layout.congressmen_list_layout, congressmen);
-		    listView.setAdapter(listAdapter);
+			
+		     rankingAdapter = new RankingAdapter(this,R.layout.ranking_layout,congressmen);
+		     listAdapter = new CongressmenListAdapter(this,R.layout.congressmen_list_layout, congressmen);
+		     listView.setAdapter(listAdapter);
+		     listAdapter.notifyDataSetChanged();
+		     listView.setTextFilterEnabled(false);
 		    //final LayoutTransition transitioner = new LayoutTransition();
 		    //transitioner.enableTransitionType(LayoutTransition.APPEARING);
 		    //listView.setLayoutTransition(transitioner);
@@ -75,6 +102,108 @@ public class ListScreen extends Activity
 					
 				}
 			});
+		    
+		    
+		    btn_search.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					 onSearchRequested();
+					
+				
+					 					
+				}
+			});
 
 	}
+	/*
+	@Override
+	public boolean onSearchRequested() {
+	  //   ListScreen.TEXT_SERVICES_MANAGER_SERVICE.g
+	   //  startSearch(null, false, appData, false);
+	     return true;
+	 }
+	
+	public void onNewIntent(Intent intent) { 
+        setIntent(intent); 
+        handleIntent(intent); 
+     } 
+
+     public void onListItemClick(ListView l, View v, int position, long id) { 
+        // call the appropriate detail activity
+     } 
+
+     private void handleIntent(Intent intent) { 
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) { 
+            query = intent.getStringExtra(SearchManager.QUERY); 
+           doSearch(query); 
+        } 
+     }    
+
+     private void doSearch(String queryStr) { 
+    	 Log.e("entrei no dosearch", "pesquisei agora");
+    	
+    	congressmanController.getByName(queryStr);
+    	listAdapter.clear();
+    	listAdapter.addAll(congressmanController.getByName(queryStr));
+    	listAdapter.notifyDataSetChanged();
+    	 
+    	 
+    	  	 
+         
+     }
+     */
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu){
+    	 MenuInflater inflater = getMenuInflater();
+    	 inflater.inflate(R.menu.menu, menu);
+    	 
+    	 SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    	
+    	 
+    	 
+    	  search = (SearchView) menu.findItem(R.id.action_search).getActionView();
+    	  
+    	 search.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    	 search.setIconifiedByDefault(false);
+    	 search.setSubmitButtonEnabled(true);
+    	 search.setOnQueryTextListener(new OnQueryTextListener() {
+			
+			@Override
+			public boolean onQueryTextSubmit(String newText) {
+				
+				return false;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				Log.e(newText, "peguei" );
+				
+				listAdapter.getFilter().filter(newText);
+				
+				Log.e(listAdapter.getItem(0).getNameCongressman(),"view");
+					return true;
+			}
+		});
+    	 return super.onCreateOptionsMenu(menu);
+     }
+	
+	private String filter (String source, int start, int end ){
+		StringBuilder sb = new StringBuilder(end - start);
+		for(int i = start; i < end; i++){
+			char c = source.charAt(i);
+			if(isCharAllowed(c))
+				sb.append(c);
+			else{
+				// do nothing here
+			}
+		}
+		String sp = new String(sb);
+		return sp;
+	}
+	
+	private boolean isCharAllowed(char c){
+		return Character.isLetterOrDigit(c)||Character.isSpace(c);
+	}
+	
 }
