e14781beb148edb78f4bbe98cf9ab0f53067d4f4
Luiz Oliveira <ziuloliveira@gmail.com>
2014-11-22 12:28:46 -0200

    Unique instance of NullUrlDao

--- src/br/com/visualize/akan/api/dao/NullUrlDao.java
+++ src/br/com/visualize/akan/api/dao/NullUrlDao.java
@@ -3,9 +3,27 @@ package br.com.visualize.akan.api.dao;
 import android.content.Context;
 
 public class NullUrlDao extends UrlDao {
-
+	private static NullUrlDao instanceUrlDao = null;
 
 	protected NullUrlDao(Context context) {
 		super(context);
+	}
+
+	/**
+	 * Return the unique instance of UrlDao active in the project.
+	 * <p>
+	 * @return The unique instance of NullUrlDao.
+	 */
+	public static NullUrlDao getInstance(Context context) {
+
+		if( instanceUrlDao != null ) {
+			/*! Nothing To Do. */
+
+		} else {
+			instanceUrlDao = new NullUrlDao(context);
+
+		}
+
+		return instanceUrlDao;
 	}
 }
