a0f197171b12fbd6271524ebd341d315ba44d716
Luiz Oliveira <ziuloliveira@gmail.com>
2014-11-22 11:25:14 -0200

    Unique instance to NullQuotaDao

--- src/br/com/visualize/akan/api/dao/NullQuotaDao.java
+++ src/br/com/visualize/akan/api/dao/NullQuotaDao.java
@@ -1,10 +1,32 @@
 package br.com.visualize.akan.api.dao;
 
+import android.content.Context;
+
 public class NullQuotaDao extends Dao{
+	private static NullQuotaDao instanceNullQuotaDao = null;
+
+	private NullQuotaDao (Context context) {
+		this.context = context;
+	}
 
 	@Override
 	protected boolean checkEmptyLocalDb() {
 		return false;
 	}
 
+
+	/**
+	 * Return the unique instance of QuotaDao active in the project.
+	 * <p>
+	 * @return The unique instance of QuotaDao.
+	 */
+	public static NullQuotaDao getInstance(Context context) {
+		if( instanceNullQuotaDao != null ) {
+			/*! Nothing To Do. */
+
+		} else {
+			instanceNullQuotaDao = new NullQuotaDao(context);
+		}
+		return instanceNullQuotaDao;
+	}
 }
