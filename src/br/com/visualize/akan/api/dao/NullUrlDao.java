e44d304c9fba5d07e2537de2c55ac14e66ff4db1
Luiz Oliveira <ziuloliveira@gmail.com>
2014-11-22 12:26:26 -0200

    Basic NullUrlDao

---
+++ src/br/com/visualize/akan/api/dao/NullUrlDao.java
@@ -0,0 +1,11 @@
+package br.com.visualize.akan.api.dao;
+
+import android.content.Context;
+
+public class NullUrlDao extends UrlDao {
+
+
+	protected NullUrlDao(Context context) {
+		super(context);
+	}
+}
--- src/br/com/visualize/akan/api/dao/UrlDao.java
+++ src/br/com/visualize/akan/api/dao/UrlDao.java
@@ -1,6 +1,6 @@
 /*
  * File: 	Url.java
- * Purpose: Bringing the implementation of modelUrl, a class that 
+ * Purpose: Bringing the implementation of modelUrl, a class that
  * directly references the business domain.
  */
 package br.com.visualize.akan.api.dao;
@@ -17,40 +17,40 @@ public class UrlDao extends Dao{
 
 	private int idUpdateUrl = 0;
 	private int updateVerifierUrl = 0;
-	
+
 	private String defaultUrl = "http://192.168.1.4:3000";
 	private String tableName = "URL";
-	
-	
+
+
 	private static UrlDao instanceUrlDao = null;
 	private static String tableUrl = "Url";
 	private static String tableColumns[] = {"ID_URL","ID_UPDATE_URL", "DEFAULT_URL", "FIRST_URL", "SECOND_URL"};
 
 
-	private UrlDao(Context context) {
+	protected UrlDao(Context context) {
 		UrlDao.database = new DatabaseHelper(context);
 	}
-	
+
 	/**
 	 * Return the unique instance of UrlDao active in the project.
 	 * <p>
 	 * @return The unique instance of UrlDao.
 	 */
 	public static UrlDao getInstance(Context context) {
-		
+
 		if( instanceUrlDao != null ) {
 			/*! Nothing To Do. */
-			
+
 		} else {
 			instanceUrlDao = new UrlDao(context);
-			
+
 		}
-		
+
 		return instanceUrlDao;
 	}
 	/**
 	 * Checks if database is empty
-	 * 
+	 *
 	 */
 	protected boolean checkEmptyLocalDb(){
 		sqliteDatabase = database.getReadableDatabase();
@@ -66,16 +66,16 @@ public class UrlDao extends Dao{
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Inserts in the database Url, referring to a congressman in particular, 
+	 * Inserts in the database Url, referring to a congressman in particular,
 	 * passed as parameter in the local database of the application.
 	 * <p>
 	 * @param insertedUrls List of Url to be inserted.
 	 */
 	public void insertUrl(Url url) {
 		sqliteDatabase = database.getWritableDatabase();
-		
+
 		ContentValues content = new ContentValues();
 
 		content.put("ID_UPDATE_URL", url.getIdUpdateUrl());
@@ -85,12 +85,12 @@ public class UrlDao extends Dao{
 		content.put("SECOND_ALTERNATIVE_URL", url.getSecondAlternativeUrl());
 		insertAndClose(sqliteDatabase, tableUrl, content);
 	}
-	
+
 	/**
-	 * Deletes all Urls of the database relating to the past as parameter 
+	 * Deletes all Urls of the database relating to the past as parameter
 	 * congressman for his numerical identifier.
 	 * <p>
-	 * @param idUrl Numeric identifier of congressman that must have 
+	 * @param idUrl Numeric identifier of congressman that must have
 	 * 		 					deleted the Urls.
 	 */
 	public void deleteUrl(Url url) {
@@ -98,12 +98,12 @@ public class UrlDao extends Dao{
 		sqliteDatabase.delete(tableUrl,"ID_UPDATE_URL=?", new String[] { url.getIdUpdateUrl() + "" });
 		sqliteDatabase.close();
 	}
-	
+
 	/**
-	 * Search the database all Urls related to the referenced congressman 
+	 * Search the database all Urls related to the referenced congressman
 	 * and returns them as a list.
 	 * <p>
-	 * @param idCongressman Numeric identifier of congressman that must have 
+	 * @param idCongressman Numeric identifier of congressman that must have
 	 * 		 					deleted the Urls.
 	 * <p>
 	 * @return The list of referenced Urls belonging to the congressman.
@@ -112,12 +112,12 @@ public class UrlDao extends Dao{
 		Url url = new Url(0,defaultUrl);
 		if (checkEmptyLocalDb()) {
 			sqliteDatabase = database.getWritableDatabase();
-			
+
 			ContentValues content = new ContentValues();
 			content.put(tableColumns[0], 1);
 			content.put(tableColumns[1], 0);
 			content.put(tableColumns[2], defaultUrl);
-			
+
 			insertAndClose(sqliteDatabase, tableName, content);
 		}
 		else{
