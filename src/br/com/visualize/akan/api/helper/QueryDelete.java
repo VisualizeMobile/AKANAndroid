371a50aaa0bc72c56cc357935471b87904391252
TomazMartins <tomaz.unb@outlook.com>
2014-11-26 23:53:40 -0200

    Implementing class QueryDelete

--- src/br/com/visualize/akan/api/helper/QueryDelete.java
+++ src/br/com/visualize/akan/api/helper/QueryDelete.java
@@ -1,22 +1,17 @@
 package br.com.visualize.akan.api.helper;
 
-public class QueryUpdate extends Query implements QueryStrategy {
+public class QueryDelete extends Query implements QueryStrategy {
 
 	@Override
 	public String doOperation(String tableName, String[] columnNames,
 			String[] values, String columnReference, String valueReference,
 			String valueComparison, String columnOrdered, String orderType) {
-		
-		StringBuilder query = new StringBuilder( "UPDATE" );
+
+		StringBuilder query = new StringBuilder( "DELETE FROM" );
 		
 		query.append(BLANK);
 		query.append(tableName);
 		query.append(BLANK);
-		query.append(SET);
-		query.append(BLANK);
-		
-		String setList = buildSetList(columnNames, columnNames);
-		query.append(setList);
 		
 		query.append(WHERE);
 		query.append(BLANK);
