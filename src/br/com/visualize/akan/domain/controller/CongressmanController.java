1b3f49c76ff9600cb37029ab63ffbb97ab07fd31
TomazMartins <tomaz.unb@outlook.com>
2014-12-22 10:41:10 -0200

    Solving conflicts

--- src/br/com/visualize/akan/domain/controller/CongressmanController.java
+++ src/br/com/visualize/akan/domain/controller/CongressmanController.java
@@ -1,95 +1,164 @@
+/*
+ * File: CongressmanController.java 
+ * Purpose: Brings the implementation of class CongressmanController.
+ */
 package br.com.visualize.akan.domain.controller;
+
 
 import java.util.List;
 
 import org.apache.http.client.ResponseHandler;
 
 import android.content.Context;
-import android.util.Log;
 import br.com.visualize.akan.api.dao.CongressmanDao;
 import br.com.visualize.akan.api.helper.JsonHelper;
 import br.com.visualize.akan.api.request.HttpConnection;
 import br.com.visualize.akan.domain.model.Congressman;
 
-public class CongressmanController {
 
+/**
+ * Serves to define the methods that are responsible for generating actions,
+ * calculate results and everything that is requested by layer 'View' referring
+ * to Congressman.
+ */
+public class CongressmanController {
+	
 	private static CongressmanController instance = null;
 	private static Congressman congressman;
 	private static List<Congressman> congressmanList;
-
+	
+	@SuppressWarnings( "unused" )
 	private Context context;
+	
+	@SuppressWarnings( "unused" )
 	private QuotaController quotaController;
+	
 	private UrlController urlController;
 	private CongressmanDao congressmanDao;
 	
-	private CongressmanController(Context context) {
-		quotaController = QuotaController.getInstance(context);
-		congressmanDao = CongressmanDao.getInstance(context);
-		urlController = UrlController.getInstance(context);
-
+	private CongressmanController( Context context ) {
+		quotaController = QuotaController.getInstance( context );
+		congressmanDao = CongressmanDao.getInstance( context );
+		urlController = UrlController.getInstance( context );
+		
 		this.context = context;
 	}
-
+	
+	/**
+	 * Return the unique instance of CongressmanController active in the project.
+	 * <p>
+	 * @return The unique instance of CongressmanController.
+	 */
 	public static CongressmanController getInstance( Context context ) {
-		if ( instance == null ) {
+		if( instance == null ) {
 			instance = new CongressmanController( context );
+		} else {
+			/*! Nothing To Do. */
 		}
 		
 		return instance;
 	}
 	
-	public void setCongressman( Congressman congressman){
+	/**
+	 * Associates a congressman on CongressmanController.
+	 * <p>
+	 * @param congressman Congressman to be associated
+	 */
+	public void setCongressman( Congressman congressman ) {
 		CongressmanController.congressman = congressman;
 	}
 	
-	public List<Congressman> requestAllCongressman( 
-			ResponseHandler<String> responseHandler )
-			throws Exception {
+	/**
+	 * Search the database all congressman and returns them as a list.
+	 * <p>
+	 * @param responseHandler Handler of welcoming server responses
+	 * <p>
+	 * @return List of Congressman resulting from the server.
+	 * <p>
+	 * @throws Exception
+	 */
+	public List<Congressman> requestAllCongressman(
+	      ResponseHandler<String> responseHandler ) throws Exception {
 		
-		if ( responseHandler != null ) {
+		if( responseHandler != null ) {
 			
-			if ( congressmanDao.checkEmptyLocalDb() ) {
-
+			if( congressmanDao.checkEmptyLocalDb() ) {
+				
 				String url = urlController.getAllCongressmanUrl();
-				String jsonCongressman = HttpConnection.request(
-						responseHandler, url );
-				Log.e(jsonCongressman, "peguei json parlamentar");
+
+				String jsonCongressman = HttpConnection.request( responseHandler,
+				      url );
+				
 				setCongressmanList( JsonHelper
-						.listCongressmanFromJSON( jsonCongressman ) );
-			
+				      .listCongressmanFromJSON( jsonCongressman ) );
 				
 				congressmanDao.insertAllCongressman( getCongressmanList() );
 				
 			} else {
-				
-				/*! Nothing To Do.*/
+				/* ! Nothing To Do. */
 			}
 		}
 		
 		return getCongressmanList();
 	}
 	
-	public static List<Congressman> getCongressmanList() {
-		
+	/*
+	 * TODO: Investigate Duplication.
+	 * 
+	 * 	It seems that the methods getCongressmanList() and getAllCongressman() 
+	 * 	are repeated. It should be investigated whether there is duplication.
+	 */
+	
+	/**
+	 * Returns all congressman stored in the local database.
+	 * <p>
+	 * @return List of Congressman.
+	 */
+	public static List<Congressman> getCongressmanList() {	
 		return congressmanList;
 	}
 	
+	/**
+	 * Returns all congressman stored in the local database.
+	 * <p>
+	 * @return List of Congressman.
+	 */
 	public List<Congressman> getAllCongressman() {
 		congressmanList = congressmanDao.getAll();
 		
 		return congressmanList;
 	}
-
+	
+	/**
+	 * Associates a list of congressman on CongressmanController.
+	 * <p>
+	 * @param congressmanList List of congressman to be associated
+	 */
 	private static void setCongressmanList( List<Congressman> congressmanList ) {
 		CongressmanController.congressmanList = congressmanList;
 	}
 	
-	public List<Congressman> getByName ( String congressmanName ) {
-		congressmanList = congressmanDao.selectCongressmanByName( congressmanName );
+	
+	/**
+	 * Returns all congressman that match the search and are stored in the 
+	 * local database.
+	 * <p>
+	 * @param congressmanName name of congressman to be searched.
+	 * <p>
+	 * @return List of congressman that match this search by name.
+	 */
+	public List<Congressman> getByName( String congressmanName ) {
+		congressmanList = congressmanDao
+		      .selectCongressmanByName( congressmanName );
 		
 		return congressmanList;
 	}
 	
+	/**
+	 * Returns the congressman associated with CongressmanController.
+	 * <p>
+	 * @return Congressman associated with CongressmanController.
+	 */
 	public Congressman getCongresman() {
 		return CongressmanController.congressman;
 	}
