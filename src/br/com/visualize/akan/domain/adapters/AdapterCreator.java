991b6ef5ed455e58d4567534797953b4ad672bea
Thaiane <thaiane.unb@hotmail.com>
2014-12-22 17:55:50 -0200

    Applying the code convention in package Adapters

--- src/br/com/visualize/akan/domain/adapters/AdapterCreator.java
+++
@@ -1,9 +0,0 @@
-package br.com.visualize.akan.domain.adapters;
-
-import android.widget.ArrayAdapter;
-
-public interface AdapterCreator<T> {
-	
-	public abstract ArrayAdapter<T> create(int typeAdapter);
-
-}
