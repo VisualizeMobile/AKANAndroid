91fd083413cfdb4df077cba67c2a36da0f4c1bfb
TomazMartins <tomaz.unb@outlook.com>
2014-12-25 17:54:58 -0200

    Applying the code convention in class JsonHelper

--- src/br/com/visualize/akan/api/helper/JsonHelper.java
+++ src/br/com/visualize/akan/api/helper/JsonHelper.java
@@ -1,80 +1,140 @@
+/*
+ * File: JsonHelper.java
+ * Purpose: Brings the implementation of class JsonHelper.
+ */
 package br.com.visualize.akan.api.helper;
+
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.util.Log;
-import br.com.visualize.akan.domain.enumeration.SubQuota;
 import br.com.visualize.akan.domain.exception.NullCongressmanException;
 import br.com.visualize.akan.domain.model.Congressman;
 import br.com.visualize.akan.domain.model.Quota;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
+/**
+ * Responsible for supporting to create JSON for server responses.
+ */
 public class JsonHelper {
 	
 	private static List<Congressman> congressmanList = new ArrayList<Congressman>();
-
-	public static List<Congressman> listCongressmanFromJSON(String jsonCongressmanList) throws NullCongressmanException, JSONException{
-		Log.e("entrei na tradução", "entrei na tradução");
-		Log.e("json aqui",jsonCongressmanList);
-		JSONArray jArray = new JSONArray(jsonCongressmanList);
-		Log.e(jArray.getJSONObject(0).getJSONObject("fields").getString("nomeparlamentar"), "peguei nome do parlamentar");
-		
 	
-		try{
+	/**
+	 * This server response to produce and return a list of Congressman.
+	 * <p>
+	 * @param jsonCongressmanList
+	 *           Representation of the JSON of Congressman list.
+	 * <p>
+	 * @return List of Congressman
+	 * <p>
+	 * @throws NullCongressmanException
+	 * @throws JSONException
+	 */
+	public static List<Congressman> listCongressmanFromJSON(
+	      String jsonCongressmanList ) throws NullCongressmanException,
+	      JSONException {
+		
+		JSONArray jArray = new JSONArray( jsonCongressmanList );
+		
+		Log.e(
+		      jArray.getJSONObject( 0 ).getJSONObject( "fields" )
+		            .getString( "nomeparlamentar" ), "Requested Congressman" );
+		
+		try {
 			Congressman congressman = null;
 			
-			for(int i = 0; i < jArray.length(); i++){
+			for( int index = 0; index < jArray.length(); index++ ) {
+				
 				congressman = new Congressman();
-				 congressman.setIdCongressman(jArray.getJSONObject(i).getInt("pk"));
-				 congressman.setTotalSpentCongressman(jArray.getJSONObject(i).getJSONObject("fields").getDouble("valor"));
-				 congressman.setRankingCongressman(jArray.getJSONObject(i).getJSONObject("fields").getInt("ranking"));
-				 congressman.setUfCongressman(jArray.getJSONObject(i).getJSONObject("fields").getString("ufparlamentar"));
-				 congressman.setPartyCongressman(jArray.getJSONObject(i).getJSONObject("fields").getString("partidoparlamentar"));
-				 congressman.setNameCongressman(jArray.getJSONObject(i).getJSONObject("fields").getString("nomeparlamentar"));
-				 congressmanList.add(congressman);
-				 Log.e(congressman.getNameCongressman(), "Peguei parlamentar do servidor");
-				  }
+				
+				congressman.setIdCongressman( jArray.getJSONObject( index ).getInt(
+				      "pk" ) );
+				
+				congressman.setTotalSpentCongressman( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getDouble( "valor" ) );
+				
+				congressman.setRankingCongressman( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "ranking" ) );
+				
+				congressman.setUfCongressman( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getString( "ufparlamentar" ) );
+				
+				congressman.setPartyCongressman( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getString( "partidoparlamentar" ) );
+				
+				congressman.setNameCongressman( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getString( "nomeparlamentar" ) );
+				
+				congressmanList.add( congressman );
+			}
 			
-		}catch(NullPointerException e){
+		} catch( NullPointerException npe ) {
 			throw new NullCongressmanException();
 		}
-		Log.e(congressmanList.get(0).getNameCongressman(), "peguei parlamentar");
+		
 		return congressmanList;
 	}
 	
-	public static List<Quota> listQuotaByIdCongressmanFromJSON(String jsonQuotaByIdCongressmanList) throws NullCongressmanException, JSONException{
+	/**
+	 * This server response to produce and return a list of Quotas.
+	 * <p>
+	 * @param jsonQuotaByIdCongressmanList
+	 *           Representation of the JSON of Quotas list.
+	 * <p>
+	 * @return List of Quotas
+	 * <p>
+	 * @throws NullCongressmanException
+	 * @throws JSONException
+	 */
+	public static List<Quota> listQuotaByIdCongressmanFromJSON(
+	      String jsonQuotaByIdCongressmanList ) throws NullCongressmanException,
+	      JSONException {
+		
 		List<Quota> quotaList = new ArrayList<Quota>();
+		
 		Quota quota;
-		JSONArray jArray = new JSONArray(jsonQuotaByIdCongressmanList);
-		try{
+		JSONArray jArray = new JSONArray( jsonQuotaByIdCongressmanList );
+		
+		try {
 			
-			
-			for ( int i = 0; i < jArray.length() ; i++ ){
+			for( int index = 0; index < jArray.length(); index++ ) {
 				quota = new Quota();
-				quota.setIdQuota(jArray.getJSONObject(i).getInt("pk"));
-				quota.setIdUpdateQuota(jArray.getJSONObject(i).getJSONObject("fields").getInt("versaoupdate"));
-				quota.setYearReferenceQuota(jArray.getJSONObject(i).getJSONObject("fields").getInt("ano"));
-				quota.setValueQuota(jArray.getJSONObject(i).getJSONObject("fields").getDouble("valor"));
-				quota.setIdCongressmanQuota(jArray.getJSONObject(i).getJSONObject("fields").getInt("idparlamentar"));
-				quota.setTypeQuotaByNumber(jArray.getJSONObject(i).getJSONObject("fields").getInt("numsubcota"));
-				quota.setDescriptionQuota(jArray.getJSONObject(i).getJSONObject("fields").getString("descricao"));
-				quota.setTypeMonthByNumber(jArray.getJSONObject(i).getJSONObject("fields").getInt("mes"));
-				Log.e(quota.getDescriptionQuota(), "Peguei ");
-				quotaList.add(quota);
+				
+				quota.setIdQuota( jArray.getJSONObject( index ).getInt( "pk" ) );
+				
+				quota.setIdUpdateQuota( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "versaoupdate" ) );
+				
+				quota.setYearReferenceQuota( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "ano" ) );
+				
+				quota.setValueQuota( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getDouble( "valor" ) );
+				
+				quota.setIdCongressmanQuota( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "idparlamentar" ) );
+				
+				quota.setTypeQuotaByNumber( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "numsubcota" ) );
+				
+				quota.setDescriptionQuota( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getString( "descricao" ) );
+				
+				quota.setTypeMonthByNumber( jArray.getJSONObject( index )
+				      .getJSONObject( "fields" ).getInt( "mes" ) );
+				
+				quotaList.add( quota );
 			}
 			
-			Log.e(quotaList.get(0).getDescriptionQuota(), "peguei quota");
-		}catch(NullPointerException e){
+		} catch( NullPointerException npe ) {
 			throw new NullCongressmanException();
 		}
+		
 		return quotaList;
 	}
-
 }
