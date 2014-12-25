13598443ec70aec9a8301db737bba87d28a29e18
TomazMartins <tomaz.unb@outlook.com>
2014-12-25 18:26:00 -0200

    Applying the code convention in class UrlDao

--- src/br/com/visualize/akan/api/dao/UrlDao.java
+++ src/br/com/visualize/akan/api/dao/UrlDao.java
@@ -1,141 +1,158 @@
 /*
- * File: 	Url.java
- * Purpose: Bringing the implementation of modelUrl, a class that
- * directly references the business domain.
+ * File: UrlDao.java 
+ * Purpose: Bringing the implementation of model URL, a class
+ * that directly references the business domain.
  */
 package br.com.visualize.akan.api.dao;
+
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import br.com.visualize.akan.api.helper.DatabaseHelper;
 import br.com.visualize.akan.domain.model.Url;
 
-public class UrlDao extends Dao{
 
-	private int idUpdateUrl = 0;
-	private int updateVerifierUrl = 0;
-
+/**
+ * This class represents the Data Access Object for the URL,
+ * responsible for all data base operations like selections, insertions
+ * and updates.
+ */
+public class UrlDao extends Dao {
+	
 	private String defaultUrl = "http://192.168.1.4:3000";
 	private String tableName = "URL";
 	
 	private static UrlDao instanceUrlDao = null;
 	private static String tableUrl = "Url";
-	private static String tableColumns[] = {"ID_URL",
-		"ID_UPDATE_URL", 
-		"DEFAULT_URL",
-		"FIRST_URL",
-		"SECOND_URL"
-		};
-
-
-	protected UrlDao(Context context) {
-		UrlDao.database = new DatabaseHelper(context);
+	private static String tableColumns[] = { "ID_URL", "ID_UPDATE_URL",
+	      "DEFAULT_URL", "FIRST_URL", "SECOND_URL" };
+	
+	protected UrlDao( Context context ) {
+		UrlDao.database = new DatabaseHelper( context );
 	}
-
+	
 	/**
 	 * Return the unique instance of UrlDao active in the project.
 	 * <p>
 	 * @return The unique instance of UrlDao.
 	 */
-	public static UrlDao getInstance(Context context) {
-
+	public static UrlDao getInstance( Context context ) {
+		
 		if( instanceUrlDao != null ) {
-			/*! Nothing To Do. */
-
+			/* ! Nothing To Do. */
+			
 		} else {
-			instanceUrlDao = new UrlDao(context);
-
+			instanceUrlDao = new UrlDao( context );
 		}
-
+		
 		return instanceUrlDao;
 	}
+	
 	/**
-	 * Checks if database is empty
-	 *
+	 * Checks if database is empty.
 	 */
-	protected boolean checkEmptyLocalDb(){
+	protected boolean checkEmptyLocalDb() {
 		sqliteDatabase = database.getReadableDatabase();
-
+		
 		String query = "SELECT 1 FROM " + tableName;
 		
 		Cursor cursor = sqliteDatabase.rawQuery( query, null );
-		Log.i("dataBase insetition", "url1");
-		if(cursor != null){
-			if(cursor.getCount() <= 0){
-				return true;
-			}
-		}
-		else{
-			return true;
-		}
-		return false;
-	}
 
+		boolean isEmpty = false;
+		
+		if( cursor != null ) {
+			
+			if( cursor.getCount() <= 0 ) {
+				isEmpty = true;
+
+			} else {
+				/*! Nothing To Do. */
+			}
+			
+		} else {
+			isEmpty = true;	
+		}
+		
+		return isEmpty;
+	}
+	
 	/**
-	 * Inserts in the database Url, referring to a congressman in particular,
+	 * Inserts in the database URL, referring to a congressman in particular,
 	 * passed as parameter in the local database of the application.
 	 * <p>
-	 * @param insertedUrls List of Url to be inserted.
+	 * @param url
+	 *           URL to be inserted.
 	 */
-	public void insertUrl(Url url) {
+	public void insertUrl( Url url ) {
 		sqliteDatabase = database.getWritableDatabase();
-
+		
 		ContentValues content = new ContentValues();
-
-		content.put("ID_UPDATE_URL", url.getIdUpdateUrl());
-		content.put("UPDATE_VERIFIER_URL", url.getUpdateVerifierUrl());
-		content.put("DEFAULT_URL", url.getDefaultUrl());
-		content.put("FIRST_ALTERNATIVE_URL", url.getFirstAlternativeUrl());
-		content.put("SECOND_ALTERNATIVE_URL", url.getSecondAlternativeUrl());
-		insertAndClose(sqliteDatabase, tableUrl, content);
+		
+		content.put( "ID_UPDATE_URL", url.getIdUpdateUrl() );
+		content.put( "UPDATE_VERIFIER_URL", url.getUpdateVerifierUrl() );
+		content.put( "DEFAULT_URL", url.getDefaultUrl() );
+		content.put( "FIRST_ALTERNATIVE_URL", url.getFirstAlternativeUrl() );
+		content.put( "SECOND_ALTERNATIVE_URL", url.getSecondAlternativeUrl() );
+		
+		insertAndClose( sqliteDatabase, tableUrl, content );
 	}
-
+	
 	/**
-	 * Deletes all Urls of the database relating to the past as parameter
+	 * Deletes a URL of the database relating to the past as parameter
 	 * congressman for his numerical identifier.
 	 * <p>
-	 * @param idUrl Numeric identifier of congressman that must have
-	 * 		 					deleted the Urls.
+	 * @param url
+	 *           URL to be deleted.
 	 */
-	public void deleteUrl(Url url) {
+	public void deleteUrl( Url url ) {
 		sqliteDatabase = database.getWritableDatabase();
-		sqliteDatabase.delete(tableUrl,"ID_UPDATE_URL=?", 
-				new String[] { url.getIdUpdateUrl() + "" });
+		
+		sqliteDatabase.delete( tableUrl, "ID_UPDATE_URL=?",
+		      new String[] { url.getIdUpdateUrl() + "" } );
+		
 		sqliteDatabase.close();
 	}
-
+	
 	/**
-	 * Search the database all Urls related to the referenced congressman
-	 * and returns them as a list.
+	 * Search the database all URLs related to the referenced congressman and
+	 * returns them as a list.
 	 * <p>
-	 * @param idCongressman Numeric identifier of congressman that must have
-	 * 		 					deleted the Urls.
-	 * <p>
-	 * @return The list of referenced Urls belonging to the congressman.
+	 * @return The list of referenced URLs belonging to the congressman.
 	 */
-	public Url getUrl(){
-		Url url = new Url(0,defaultUrl);
-		if (checkEmptyLocalDb()) {
+	public Url getUrl() {
+		Url url = new Url( 0, defaultUrl );
+		
+		if( checkEmptyLocalDb() ) {
 			sqliteDatabase = database.getWritableDatabase();
-
+			
 			ContentValues content = new ContentValues();
-			content.put(tableColumns[0], 1);
-			content.put(tableColumns[1], 0);
-			content.put(tableColumns[2], defaultUrl);
-
-			insertAndClose(sqliteDatabase, tableName, content);
-		}
-		else{
+			
+			content.put( tableColumns[ 0 ], 1 );
+			content.put( tableColumns[ 1 ], 0 );
+			content.put( tableColumns[ 2 ], defaultUrl );
+			
+			insertAndClose( sqliteDatabase, tableName, content );
+			
+		} else {
 			sqliteDatabase = database.getReadableDatabase();
-			Cursor cursor = sqliteDatabase.rawQuery("SELECT * FROM URL", null);
+			
+			Cursor cursor = sqliteDatabase.rawQuery( "SELECT * FROM URL", null );
 			cursor.moveToFirst();
-			url.setIdUpdateUrl(cursor.getInt(cursor.getColumnIndex(tableColumns[1])));
-			url.setDefaultUrl(cursor.getString(cursor.getColumnIndex(tableColumns[2])));
-			url.setFirstAlternativeUrl(cursor.getString(cursor.getColumnIndex(tableColumns[3])));
-			url.setSecondAlternativeUrl(cursor.getString(cursor.getColumnIndex(tableColumns[4])));
+			
+			url.setIdUpdateUrl( cursor.getInt( cursor
+			      .getColumnIndex( tableColumns[ 1 ] ) ) );
+			
+			url.setDefaultUrl( cursor.getString( cursor
+			      .getColumnIndex( tableColumns[ 2 ] ) ) );
+			
+			url.setFirstAlternativeUrl( cursor.getString( cursor
+			      .getColumnIndex( tableColumns[ 3 ] ) ) );
+			
+			url.setSecondAlternativeUrl( cursor.getString( cursor
+			      .getColumnIndex( tableColumns[ 4 ] ) ) );
 		}
+		
 		return url;
 	}
 }
