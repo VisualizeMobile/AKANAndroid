e5868b0daa33794bbfe8df208230361727bfc2d3
MaxAlmeida <llewxam100@hotmail.com>
2014-12-25 23:06:29 -0200

    Repair requisition thread of thecongressman , aplying progress dialog

--- src/br/com/visualize/akan/domain/view/SplashScreen.java
+++ src/br/com/visualize/akan/domain/view/SplashScreen.java
@@ -8,9 +8,14 @@ package br.com.visualize.akan.domain.view;
 import org.apache.http.client.ResponseHandler;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import br.com.visualize.akan.R;
 import br.com.visualize.akan.api.request.HttpConnection;
 import br.com.visualize.akan.domain.controller.CongressmanController;
 
@@ -25,16 +30,12 @@ public class SplashScreen extends Activity {
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
-		
+		setContentView(R.layout.splash_screen_activity);
 		congressmanController = CongressmanController
 		      .getInstance( getApplicationContext() );
 		
-		new Thread( new InitialRequest() ).start();
+				requestCongressman();
 		
-		Intent myAction = new Intent( SplashScreen.this, ListScreen.class );
-		
-		SplashScreen.this.startActivity( myAction );
-		SplashScreen.this.finish();
 	}
 	
 	
@@ -42,23 +43,42 @@ public class SplashScreen extends Activity {
 	 * Providence the actions that should be made to the initial request for 
 	 * the operation of the Thread.
 	 */
-	private class InitialRequest implements Runnable {
+	public void requestCongressman(){
+		Log.e("Entrei no meodo requestCongressman","Entrei no metodo requestCongressman");
+		final ProgressDialog progress = new ProgressDialog(this);
+		progress.setMessage("Carregando dados...");
+		progress.show();
 		
-		@Override
-		public void run() {
-			Looper.prepare();
+		new Thread(){
 			
-			ResponseHandler<String> responseHandler = HttpConnection
-			      .getResponseHandler();
-			
-			try {
-				congressmanController.requestAllCongressman( responseHandler );
+			public void run(){
 				
-			} catch( Exception e ) {
-				e.printStackTrace();
-			}
+				Looper.prepare();
+				try {
+					ResponseHandler<String> responseHandler = HttpConnection
+						      .getResponseHandler();
+					congressmanController.requestAllCongressman( responseHandler );
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+				
+				runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						Log.e("Entrei trhead","Entrei thread");
+						progress.setMessage("Dados carregados");
+						Intent myAction = new Intent( SplashScreen.this, ListScreen.class );
+						SplashScreen.this.startActivity( myAction );
+						SplashScreen.this.finish();
+						progress.dismiss();
+						Looper.loop();
+					}
+				});
 			
-			Looper.loop();
-		}
+			}
+		}.start();
 	}
+	
 }
