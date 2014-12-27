bb78a61e9bede3414db6f1e78c9aeed826805c30
Thaiane <thaiane.unb@hotmail.com>
2014-12-26 23:53:34 -0200

    Applying the code convention in class Dao

--- src/br/com/visualize/akan/api/dao/Dao.java
+++ src/br/com/visualize/akan/api/dao/Dao.java
@@ -1,20 +1,45 @@
+/*
+ * File: Dao.java 
+ * Purpose: Brings the implementation of class DAO.
+ */
 package br.com.visualize.akan.api.dao;
 
-import br.com.visualize.akan.api.helper.DatabaseHelper;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import br.com.visualize.akan.api.helper.DatabaseHelper;
 
+
+/**
+ * Serves to keep details shared between classes Data Access Object.
+ */
 public abstract class Dao {
 	
 	protected static DatabaseHelper database;
 	protected static SQLiteDatabase sqliteDatabase;
 	protected static Context context;
 	
+	/**
+	 * Checks if database is empty.
+	 */
 	protected abstract boolean checkEmptyLocalDb();
 	
-	protected long insertAndClose( SQLiteDatabase sqliteDatabase, String table, 
-			ContentValues values ) {
+	/**
+	 * Opens the database so that you can insert content within a given table in
+	 * a given database present in the application.
+	 * <p>
+	 * @param sqlitedatabase
+	 *           Database where the table.
+	 * @param table
+	 *           Table to alter
+	 * @param values
+	 *           Set of values to be added.
+	 * <p>
+	 * @return the row ID of the newly inserted row, or -1 if an error occurred.
+	 */
+	protected long insertAndClose( SQLiteDatabase sqliteDatabase, String table,
+	      ContentValues values ) {
 		
 		long resultInsert = sqliteDatabase.insert( table, null, values );
 		
