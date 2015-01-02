575b27718f58a1d0533d032a3facee52526e0f0d
TomazMartins <tomaz.unb@outlook.com>
2015-01-02 17:43:56 -0200

    Creating StatisticDao

---
+++ src/br/com/visualize/akan/api/dao/StatisticDao.java
@@ -0,0 +1,39 @@
+package br.com.visualize.akan.api.dao;
+
+import android.content.Context;
+import br.com.visualize.akan.domain.enumeration.SubQuota;
+import br.com.visualize.akan.domain.model.Statistic;
+
+public class StatisticDao {
+	private static StatisticDao instanceStatisticDao = null; 
+	
+	private StatisticDao( Context context ) {
+		QuotaDao.context = context;
+	}
+	
+	/**
+	 * Return the unique instance of QuotaDao active in the project.
+	 * <p>
+	 * @return The unique instance of QuotaDao.
+	 */
+	public static StatisticDao getInstance( Context context ) {
+		if( instanceStatisticDao != null ) {
+			/* !Nothing To Do. */
+			
+		} else {
+			instanceStatisticDao = new StatisticDao( context );
+		}
+		
+		return instanceStatisticDao;
+	}
+	
+	public Statistic getStatisticByYear() {
+		Statistic statisticYear = new Statistic();
+		statisticYear.setAverage( 20000 );
+		statisticYear.setMaxValue( 25000 );
+		statisticYear.setStdDeviation( 200 );
+		statisticYear.setSubquota( SubQuota.ACCOMMODATION );
+		
+		return statisticYear;
+	}
+}
--- src/br/com/visualize/akan/domain/controller/StatisticController.java
+++ src/br/com/visualize/akan/domain/controller/StatisticController.java
@@ -1,9 +1,32 @@
 package br.com.visualize.akan.domain.controller;
 
+import android.content.Context;
+import br.com.visualize.akan.api.dao.StatisticDao;
 import br.com.visualize.akan.domain.model.Statistic;
 
 public class StatisticController {
-	StatisticDao statisticDao = StatisticDao.getInstance();
+	private static StatisticController instanceStatisticController = null;
+	private StatisticDao statisticDao = null;
+	
+	private StatisticController( Context context ) {
+		statisticDao = StatisticDao.getInstance( context );
+	}
+	
+	/**
+	 * Return the unique instance of QuotaDao active in the project.
+	 * <p>
+	 * @return The unique instance of QuotaDao.
+	 */
+	public static StatisticController getInstance( Context context ) {
+		if( instanceStatisticController != null ) {
+			/* !Nothing To Do. */
+			
+		} else {
+			instanceStatisticController = new StatisticController( context );
+		}
+		
+		return instanceStatisticController;
+	}
 	
 	public Statistic getStatisticByYear() {
 		Statistic statisticYear = statisticDao.getStatisticByYear();
