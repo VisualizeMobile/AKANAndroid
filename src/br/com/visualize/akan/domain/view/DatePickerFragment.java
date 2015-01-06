ae7e2d17d1eec4101fa368aeb7309bfaef859b71
MaxAlmeida <llewxam100@hotmail.com>
2015-01-06 16:13:41 -0200

    Removing day spinner from datepicker

--- src/br/com/visualize/akan/domain/view/DatePickerFragment.java
+++ src/br/com/visualize/akan/domain/view/DatePickerFragment.java
@@ -1,11 +1,13 @@
 package br.com.visualize.akan.domain.view;
 
+import java.text.DateFormat.Field;
 import java.util.Calendar;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 import android.widget.DatePicker;
 
 public class DatePickerFragment extends DialogFragment implements 
@@ -18,15 +20,54 @@ DatePickerDialog.OnDateSetListener{
 	        int year = c.get(Calendar.YEAR);
 	        int month = c.get(Calendar.MONTH);
 	        int day = c.get(Calendar.DAY_OF_MONTH);
-
+	        DatePickerDialog datepicker = new DatePickerDialog(getActivity(), this, year, month, day);
+	        datepicker.setTitle("Período");
+	        hideyearCalendar(datepicker);
 	        // Create a new instance of DatePickerDialog and return it
-	        return new DatePickerDialog(getActivity(), this, year, month, day);
+	        return datepicker;
 	    }
-
+	 
 		@Override
 		public void onDateSet(DatePicker view, int year, int monthOfYear,
 				int dayOfMonth) {
-			// TODO Auto-generated method stub
+			
 			
 		}
+		
+		private Dialog hideyearCalendar(DatePickerDialog datePickerDialog) {
+		    try
+		    {
+		         java.lang.reflect.Field[] datePickerDialogFields = datePickerDialog.getClass().getDeclaredFields();
+		         for (java.lang.reflect.Field datePickerDialogField : datePickerDialogFields) { 
+		                if (datePickerDialogField.getName().equals("mDatePicker")) {
+		                    datePickerDialogField.setAccessible(true);
+		                    DatePicker datePicker = (DatePicker) datePickerDialogField.get(datePickerDialog);
+		                    java.lang.reflect.Field[] datePickerFields = datePickerDialogField.getType().getDeclaredFields();
+		                    for (java.lang.reflect.Field datePickerField : datePickerFields) {
+		                       if ("mDayPicker".equals(datePickerField.getName())|"mDaySpinner".equals(datePickerField.getName())) {
+		                          datePickerField.setAccessible(true);
+		                          Object dayPicker= new Object();
+		                          dayPicker = datePickerField.get(datePicker);
+		                          ((View) dayPicker).setVisibility(View.GONE);
+		                          
+		                       }
+		                       datePickerDialog.setTitle("Periodo");
+		                    }
+		                 }
+
+		              }
+		        
+		         datePickerDialog.getDatePicker().setCalendarViewShown(false);
+		        return datePickerDialog;    
+		    }
+		    catch(Exception e)
+		    {
+		        
+		    return datePickerDialog;    
+		        
+		    }
+		    
+		}
+
+		
 	}
