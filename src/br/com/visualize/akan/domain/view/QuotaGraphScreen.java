6c877cbb8669784611329a8d5f31225d726c258e
ArthurJahn <stutrzbecher@gmail.com>
2015-06-04 21:33:37 -0300

    Adding quota graph activity

---
+++ src/br/com/visualize/akan/domain/view/QuotaGraphScreen.java
@@ -0,0 +1,15 @@
+package br.com.visualize.akan.domain.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.visualize.akan.R;
+
+public class QuotaGraphScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quota_graph_screen);
+	}
+
+}
